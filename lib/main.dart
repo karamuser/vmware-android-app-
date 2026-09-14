d7eae4c1fdc5bd9ecb5ae8a95e21f585437ac9e6
karamuser <albarekaram206@gmail.com>
2026-09-14 16:25:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const VirtualMachineApp());
+}
+
+class VirtualMachineApp extends StatelessWidget {
+  const VirtualMachineApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'محاكي الأنظمة المحمول',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF0F172A),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF1E293B),
+          elevation: 0,
+          centerTitle: true,
+        ),
+        cardColor: const Color(0xFF1E293B),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
 
