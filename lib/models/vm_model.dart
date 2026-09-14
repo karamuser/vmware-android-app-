f99e406d6eefe2b9acb2b9513777439445cc71ca
karamuser <albarekaram206@gmail.com>
2026-09-14 16:17:48 +0300

    Update vm_model.dart

--- lib/models/vm_model.dart
+++ lib/models/vm_model.dart
@@ -1,1 +1,26 @@
+class VirtualMachine {
+  String id;
+  String name;
+  String isoPath;
+  int ramMb;
+  int cpuCores;
+  String architecture; // x86_64, arm64, i386
+  int diskSizeGb;
+  bool enableAudio;
+  bool enableVirtualKeyboard;
+  bool enableTouchMouse;
+
+  VirtualMachine({
+    required this.id,
+    required this.name,
+    required this.isoPath,
+    this.ramMb = 2048,
+    this.cpuCores = 2,
+    this.architecture = 'x86_64',
+    this.diskSizeGb = 20,
+    this.enableAudio = true,
+    this.enableVirtualKeyboard = true,
+    this.enableTouchMouse = true,
+  });
+}
 
